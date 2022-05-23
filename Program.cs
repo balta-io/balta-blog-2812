1be3294ac54bef62bf2b4b7ff617265774a9e956
Andre Baltieri <andrebaltieri@hotmail.com>
2022-05-23 13:33:05 -0300

    Alterado a mensagem da API

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello my change is online!");
 
 app.Run();
