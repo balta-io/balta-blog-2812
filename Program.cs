345f07ac208d4dddc3cbe4b81ce698bee616213e
Andre Baltieri <andrebaltieri@hotmail.com>
2022-05-24 13:45:14 -0300

    Alterado as configuracoes do app

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello my change is online!");
+var connectionString =
+    app.Configuration.GetConnectionString("DefaultConnection");
+
+var secrets = new Secrets();
+app.Configuration.GetSection("Secrets").Bind(secrets);
+
+app.MapGet("/", () => new
+{
+    ConnectionString = connectionString,
+    Secrets = secrets
+});
 
 app.UseStaticFiles();
 
 app.Run();
+
+public class Secrets
+{
+    public string JwtTokenSecret { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+    public string PrivateKey { get; set; } = string.Empty;
+}
